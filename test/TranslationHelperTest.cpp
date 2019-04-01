58d9db92a0960315d9aae07b3bb19d285ffc6a44
Andreas Stefl <stefl.andreas@gmail.com>
2019-04-01 08:11:24 +0200

    add specific entry translation

--- test/TranslationHelperTest.cpp
+++ test/TranslationHelperTest.cpp
@@ -10,10 +10,12 @@ namespace odr {
 TEST(TranslationHelperTest, translate) {
     std::string input;
     input = "/home/andreas/workspace/OpenDocument.test/files/spreadsheet/efficiency-big-2.ods";
-    input = "/home/andreas/Desktop/odr/ruski.odt";
+    input = "/home/andreas/Desktop/odr/test.odp";
     const std::string output = "../../test/test.html";
 
     TranslationConfig config = {};
+    config.entryOffset = 0;
+    config.entryCount = 0;
     auto translator = TranslationHelper::create();
     translator->open(input);
     translator->translate(output, config);
