8097c36803ae585da3f762cb5bd0542aeb7e28d4
Andreas Stefl <stefl.andreas@gmail.com>
2020-01-12 16:01:35 +0100

    fix test

--- test/OpenDocumentMetaTest.cpp
+++ test/OpenDocumentMetaTest.cpp
@@ -9,7 +9,7 @@ TEST(OpenDocumentMetaTest, open) {
     const std::string path = "../../test/empty.odp";
 
     odr::ZipReader odf(path);
-    const odr::FileMeta meta = odr::OpenDocumentMeta::parseFileMeta(odf);
+    const odr::FileMeta meta = odr::OpenDocumentMeta::parseFileMeta(odf, false);
 
     LOG(INFO) << (int) meta.type;
     LOG(INFO) << meta.entryCount;
