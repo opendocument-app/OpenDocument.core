a9a6cdea63ffbd408c122390354505f37e0709f2
Andreas Stefl <stefl.andreas@gmail.com>
2020-02-01 22:41:31 +0100

    docx fix paragraph vs span, add text alignment, color, decoration and start with tables

--- src/ooxml/MicrosoftStyleTranslator.h
+++ src/ooxml/MicrosoftStyleTranslator.h
@@ -2,6 +2,7 @@
 #define ODR_MICROSOFTSTYLETRANSLATOR_H
 
 #include <memory>
+#include <ostream>
 
 namespace tinyxml2 {
 class XMLElement;
@@ -14,6 +15,7 @@ struct TranslationContext;
 class MicrosoftStyleTranslator final {
 public:
     static void translate(const tinyxml2::XMLElement &in, TranslationContext &context);
+    static void translateInline(const tinyxml2::XMLElement &in, std::ostream &out, TranslationContext &context);
 };
 
 }
