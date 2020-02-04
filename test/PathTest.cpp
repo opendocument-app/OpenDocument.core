cde6801af50147e05edace025262f0a476c0552c
Andreas Stefl <stefl.andreas@gmail.com>
2020-02-04 18:01:28 +0100

    enhance path

--- test/PathTest.cpp
+++ test/PathTest.cpp
@@ -6,43 +6,14 @@
 namespace odr {
 
 TEST(Path, empty) {
-    Path a;
-    Path b = "a";
-    LOG(INFO) << a.hash();
-    LOG(INFO) << b.hash();
-    LOG(INFO) << (a == b);
-    LOG(INFO) << (a != b);
-    LOG(INFO) << (a < b);
+    EXPECT_EQ("", Path().string());
 }
 
-TEST(Path, unordered_map) {
-    std::unordered_map<Path, int> map;
+TEST(Path, join) {
+    Path a("ppt/slides");
+    Path b("../media/image8.png");
 
-    map["a"] = 1;
-    map["b"] = 2;
-    map[""] = 3;
-    map["c/d"] = 4;
-    map["a"] = 5;
-
-    for (const auto &i : map) {
-        LOG(INFO) << i.first << " " << i.second;
-    }
-}
-
-TEST(Path, nestedAssign) {
-    struct Entry {
-        Path path;
-        std::size_t sizeReal;
-        std::size_t sizeUncompressed;
-        std::string mediaType;
-        bool encrypted;
-    };
-
-    std::unordered_map<Path, Entry> map;
-
-    map["asadf"] = {};
-
-    for (const auto &i : map) LOG(INFO) << i.first;
+    EXPECT_EQ("ppt/media/image8.png", a.join(b).string());
 }
 
 }
