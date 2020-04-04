8feb8720a062a2f24250b8c160b2d52f6fc4578c
Andreas Stefl <stefl.andreas@gmail.com>
2020-04-04 20:49:08 +0200

    tmp

--- .travis/render.js
+++ .travis/render.js
@@ -4,13 +4,13 @@ var webpage = require('webpage');
 var args = system.args;
 var page = webpage.create();
 
+width = 1024;
+height = 768;
+
+page.viewportSize = { width: width, height: height };
+page.clipRect = { top: 0, left: 0, width: width, height: height };
+
 page.open(args[1], function () {
-    setTimeout(function () {
-        page.viewportSize = {width: 1024, height: 768};
-        page.evaluate(function () {
-            document.body.bgColor = 'white';
-        });
-        page.render(args[2]);
-        phantom.exit();
-    }, 200);
+    page.render(args[2]);
+    phantom.exit();
 });
