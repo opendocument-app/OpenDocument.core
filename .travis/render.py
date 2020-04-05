c2189a14a6afbdbe2d2a28a11f3313d2cad375a0
Andreas Stefl <stefl.andreas@gmail.com>
2020-04-05 12:52:50 +0200

    tmp

--- .travis/render.py
+++ .travis/render.py
@@ -19,8 +19,8 @@ def main():
 
     print('render %s to %s' % (infile, outimage))
 
-    translate = [args.translator, infile, outimage]
-    result = subprocess.run(translate, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    render = [args.renderer, infile, outimage]
+    result = subprocess.run(render, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     if result.returncode != 0 or not os.path.isfile(outimage):
       if os.path.isfile(outimage):
         os.remove(outimage)
@@ -30,7 +30,7 @@ def main():
 
   if not failed:
     return 0
-  print('translation failed for the following files:')
+  print('render failed for the following files:')
   print('\n'.join(failed))
   return 1
 
