2f8ed64f0c68092af3cdfb5e4d3aba3465a6d253
Andreas Stefl <stefl.andreas@gmail.com>
2020-04-17 13:21:41 +0200

    detect legacy ms file types

--- odr/src/Meta.cpp
+++ odr/src/Meta.cpp
@@ -8,19 +8,25 @@ std::string typeToString(const odr::FileType type) {
   case odr::FileType::COMPOUND_FILE_BINARY_FORMAT:
     return "cfb";
   case odr::FileType::OPENDOCUMENT_TEXT:
-    return "odr";
-  case odr::FileType::OPENDOCUMENT_SPREADSHEET:
-    return "ods";
+    return "odt";
   case odr::FileType::OPENDOCUMENT_PRESENTATION:
     return "odp";
+  case odr::FileType::OPENDOCUMENT_SPREADSHEET:
+    return "ods";
   case odr::FileType::OPENDOCUMENT_GRAPHICS:
     return "odg";
   case odr::FileType::OFFICE_OPEN_XML_DOCUMENT:
     return "docx";
-  case odr::FileType::OFFICE_OPEN_XML_WORKBOOK:
-    return "xlsx";
   case odr::FileType::OFFICE_OPEN_XML_PRESENTATION:
     return "pptx";
+  case odr::FileType::OFFICE_OPEN_XML_WORKBOOK:
+    return "xlsx";
+  case odr::FileType::LEGACY_WORD_DOCUMENT:
+    return "doc";
+  case odr::FileType::LEGACY_POWERPOINT_PRESENTATION:
+    return "ppt";
+  case odr::FileType::LEGACY_EXCEL_WORKSHEETS:
+    return "xls";
   default:
     return "unnamed";
   }
