8db00af8e788cb98130cd168f27f1d8a4cd61e80
Andreas Stefl <stefl.andreas@gmail.com>
2020-04-21 18:39:21 +0200

    move server to executable

--- cli/src/server.cpp
+++ cli/src/server.cpp
@@ -1,14 +1,97 @@
-#include <odr/Server.h>
 #include <string>
+#include <access/FileUtil.h>
+#include <access/Path.h>
+#include <httplib.h>
+#include <utility>
+#include <odr/Config.h>
+#include <odr/Meta.h>
+#include <odr/OpenDocumentReader.h>
+
+namespace {
+struct Config {
+  std::string tmpDir;
+  std::string htmlDir;
+};
+
+class Server final {
+public:
+  explicit Server(Config config) : config(std::move(config)) {
+    server.Get("/", [&](const auto &req, auto &res) {
+      const char *html = R"V0G0N(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>odr</title>
+</head>
+<body>
+    <form action="/app/open" method="post" enctype="multipart/form-data">
+        <input type="file" id="file" name="file">
+        <input type="submit">
+    </form>
+</body>
+</html>
+  )V0G0N";
+
+      res.set_content(html, "text/html");
+    });
+
+    server.Post("/app/open", [&](const auto &req, auto &res) {
+      if (!req.has_file("file")) {
+        res.set_content("no file", "text/plain");
+        return;
+      }
+
+      const auto &file = req.get_file_value("file");
+      const auto tmpInputFile = odr::access::Path{config.tmpDir}.join("input");
+      const auto tmpOutputFile = odr::access::Path{config.tmpDir}.join("output");
+      odr::access::FileUtil::write(file.content, tmpInputFile);
+
+      bool success{true};
+
+      success = odr.open(tmpInputFile);
+      if (!success) {
+        res.set_content("cannot open", "text/plain");
+        return;
+      }
+
+      if (odr.getMeta().encrypted) {
+        res.set_content("file encrypted", "text/plain");
+        return;
+      }
+
+      ::odr::Config odrConfig{};
+      success = odr.translate(tmpOutputFile, odrConfig);
+      if (!success) {
+        res.set_content("cannot translate", "text/plain");
+        return;
+      }
+
+      const auto html = odr::access::FileUtil::read(tmpOutputFile);
+      res.set_content(html, "text/html");
+    });
+
+    server.Post("/api/open", [&](const auto &req, auto &res) {});
+  }
+
+  void listen(const std::string &endpoint, const int port) {
+    server.listen(endpoint.c_str(), port);
+  }
+
+private:
+  const Config config;
+  httplib::Server server;
+  odr::OpenDocumentReader odr;
+};
+}
 
 int main(int argc, char **argv) {
   const std::string endpoint{"0.0.0.0"};
   const int port{std::stoi(argv[1])};
 
-  odr::ServerConfig config;
+  Config config;
   config.tmpDir = "/tmp";
 
-  odr::Server server{config};
+  Server server{config};
   server.listen(endpoint, port);
 
   return 0;
