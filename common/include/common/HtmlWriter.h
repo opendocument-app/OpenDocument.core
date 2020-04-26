c7f69d958919f13f047053c5da92eb304418ae1a
Andreas Stefl <stefl.andreas@gmail.com>
2020-04-26 19:25:25 +0200

    restructure html writer

--- common/include/common/HtmlWriter.h
+++ common/include/common/HtmlWriter.h
@@ -9,41 +9,32 @@
 namespace odr {
 namespace common {
 
-class HtmlAttributeValueWriter;
-class HtmlCommentWriter;
-class HtmlTextWriter;
-class HtmlElementWriter;
+class HtmlHeadWriter;
 class CssWriter;
+class ScriptWriter;
+class HtmlElementWriter;
 
-class HtmlWriter {
+class HtmlWriter final {
 public:
-  virtual ~HtmlWriter() = default;
-
-  void addDoctype();
-
-  void startElement(const std::string &name);
-
-  void addAttribute(const std::string &name, const std::string &value);
-
-  void addComment(const std::string &text);
-
-  void addText(const std::string &text);
-
-  void endElement(const std::string &name);
-};
-
-class HtmlDocumentWriter final {
-public:
-  explicit HtmlDocumentWriter(std::unique_ptr<access::Sink> sink);
-  ~HtmlDocumentWriter();
-
-  HtmlElementWriter root();
+  HtmlHeadWriter head(const std::string &title);
 
 private:
-  std::unique_ptr<access::Sink> sink_;
-  std::vector<std::string> stack_;
+};
 
-  void addDoctype_();
+class HtmlHeadWriter final {
+public:
+  HtmlHeadWriter meta(const std::string &charset);
+  HtmlHeadWriter meta(const std::string &name, const std::string &content);
+
+  CssWriter style();
+  HtmlHeadWriter link();
+
+  ScriptWriter script();
+  HtmlHeadWriter script(const std::string &href);
+
+  HtmlElementWriter body();
+
+private:
 };
 
 class HtmlElementWriter final {
@@ -61,7 +52,6 @@ public:
   HtmlTextWriter addText();
 
 private:
-
 };
 
 }
--- common/src/HtmlWriter.cpp
+++ common/src/HtmlWriter.cpp
@@ -3,17 +3,5 @@
 namespace odr {
 namespace common {
 
-HtmlDocumentWriter::HtmlDocumentWriter(std::unique_ptr<access::Sink> sink) : sink_(std::move(sink)) {
-  addDoctype_();
-}
-
-HtmlDocumentWriter::~HtmlDocumentWriter() {
-  sink_->write("</html>", 7);
-}
-
-void HtmlDocumentWriter::addDoctype_() {
-  sink_->write("<!DOCTYPE html>\n", 16);
-}
-
 }
 }
