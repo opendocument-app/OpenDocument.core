12830807b94abade708485c8cdb22bf4e4e8d78e
Andreas Stefl <stefl.andreas@gmail.com>
2020-05-23 16:53:40 +0200

    tmp

--- common/src/XmlUtil.cpp
+++ common/src/XmlUtil.cpp
@@ -1,5 +1,4 @@
 #include <access/Storage.h>
-#include <access/StorageUtil.h>
 #include <access/StreamUtil.h>
 #include <common/XmlUtil.h>
 #include <pugixml.hpp>
@@ -16,14 +15,20 @@ pugi::xml_document XmlUtil::parse(const std::string &in) {
 }
 
 pugi::xml_document XmlUtil::parse(std::istream &in) {
-  return parse(access::StreamUtil::read(in));
+  pugi::xml_document result;
+  const auto success = result.load(in);
+  if (!success)
+    throw NotXmlException();
+  return result;
 }
 
 pugi::xml_document
 XmlUtil::parse(const access::ReadStorage &storage, const access::Path &path) {
-  if (!storage.isReadable(path))
-    throw 123; // TODO
-  return parse(access::StorageUtil::read(storage, path));
+  pugi::xml_document result;
+  const auto success = result.load(*storage.read(path));
+  if (!success)
+    throw NotXmlException();
+  return result;
 }
 
 } // namespace common
