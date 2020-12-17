43ff66465af73035c3d7ff1b4bb830d5e0733cc6
Andreas Stefl <stefl.andreas@gmail.com>
2020-12-17 12:51:04 +0100

    tmp

--- test/scripts/compare_test_outputs.py
+++ test/scripts/compare_test_outputs.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import argparse
 import json
 from html_render_diff import get_browser, html_render_diff
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class CompareError(Exception):
+    pass
 
 
 def parse_json(path):
@@ -20,7 +37,66 @@ def compare_json(a, b):
 def compare_html(a, b):
     browser = get_browser()
     diff = html_render_diff(browser, a, b)
-    return True if diff is None else False
+    return True if diff.getbbox() is None else False
+
+
+def compare_files(a, b):
+    if a.endswith('.json'):
+        return compare_json(a, b)
+    elif a.endswith('.html'):
+        return compare_html(a, b)
+
+
+def comparable_file(path):
+    if path.endswith('.json'):
+        return True
+    if path.endswith('.html'):
+        return True
+    return False
+
+
+def compare_dirs(a, b, level=0, prefix=''):
+    prefix_file = prefix + '├── '
+    if level == 0:
+        print(f'compare dir {a} with {b}')
+
+    left = [os.path.join(a, name) for name in os.listdir(a)]
+    right = [os.path.join(a, name) for name in os.listdir(b)]
+
+    left_files = sorted([path for path in left if os.path.isfile(path) and comparable_file(path)])
+    right_files = sorted([path for path in right if os.path.isfile(path) and comparable_file(path)])
+
+    if left_files != right_files:
+        left_missing = ' '.join([os.path.basename(path) for path in right_files if path not in left_files])
+        right_missing = ' '.join([os.path.basename(path) for path in left_files if path not in right_files])
+        if left_missing:
+            print(f'{prefix_file}{bcolors.FAIL}missing files left: {left_missing} ✘{bcolors.ENDC}')
+        if right_missing:
+            print(f'{prefix_file}{bcolors.FAIL}missing files right: {right_missing} ✘{bcolors.ENDC}')
+    common_files = [path for path in left_files if path in right_files]
+
+    for filename in [os.path.basename(path) for path in common_files]:
+        cmp = compare_files(os.path.join(a, filename), os.path.join(b, filename))
+        if cmp:
+            print(f'{prefix_file}{bcolors.OKGREEN}{filename} ✓{bcolors.ENDC}')
+        else:
+            print(f'{prefix_file}{bcolors.FAIL}{filename} ✘{bcolors.ENDC}')
+
+    left_dirs = sorted([path for path in left if os.path.isdir(path)])
+    right_dirs = sorted([path for path in right if os.path.isdir(path)])
+
+    if left_dirs != right_dirs:
+        left_missing = ' '.join([os.path.basename(path) for path in right_dirs if path not in left_dirs])
+        right_missing = ' '.join([os.path.basename(path) for path in left_dirs if path not in right_dirs])
+        if left_missing:
+            print(f'{prefix_file}{bcolors.FAIL}missing dirs left: {left_missing} ✘{bcolors.ENDC}')
+        if right_missing:
+            print(f'{prefix_file}{bcolors.FAIL}missing dirs right: {right_missing} ✘{bcolors.ENDC}')
+    common_dirs = [path for path in left_dirs if path in right_dirs]
+
+    for dirname in [os.path.basename(path) for path in common_dirs]:
+        print(prefix + '├── ' + dirname)
+        compare_dirs(os.path.join(a, dirname), os.path.join(b, dirname), level=level+1, prefix=prefix + '│   ')
 
 
 def main():
@@ -28,6 +104,8 @@ def main():
     parser.add_argument('a')
     parser.add_argument('b')
     args = parser.parse_args()
+
+    compare_dirs(args.a, args.b)
 
     return 0
 
--- test/scripts/tidy_test_output.py
+++ test/scripts/tidy_test_output.py
@@ -1,17 +1,83 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import argparse
 import json
+import subprocess
+import shlex
 
 
-def parse_json(path):
-    with open(path) as f:
-        return json.load(f)
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
-def tidy_html(path):
+class HtmlTidyError(Exception):
     pass
+
+
+def tidy_json(path, **kwargs):
+    try:
+        with open(path) as f:
+            json.load(f)
+        return 0
+    except ValueError:
+        return 1
+
+
+def tidy_html(path, **kwargs):
+    result = subprocess.run(shlex.split('tidy -q %s' % path), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if result.returncode == 1:
+        return 1
+    if result.returncode > 1:
+        return 2
+    return 0
+
+
+def tidy_file(path, **kwargs):
+    if path.endswith('.json'):
+        return tidy_json(path)
+    elif path.endswith('.html'):
+        return tidy_html(path)
+
+
+def tidyable_file(path, **kwargs):
+    if path.endswith('.json'):
+        return True
+    if path.endswith('.html'):
+        return True
+    return False
+
+
+def tidy_dir(path, level=0, prefix='', **kwargs):
+    prefix_file = prefix + '├── '
+    if level == 0:
+        print(f'tidy dir {path}')
+
+    items = [os.path.join(path, name) for name in os.listdir(path)]
+    files = sorted([path for path in items if os.path.isfile(path) and tidyable_file(path)])
+    dirs = sorted([path for path in items if os.path.isdir(path)])
+
+    for filename in [os.path.basename(path) for path in files]:
+        tidy = tidy_file(os.path.join(path, filename))
+        if tidy == 0:
+            print(f'{prefix_file}{bcolors.OKGREEN}{filename} ✓{bcolors.ENDC}')
+        elif tidy == 1:
+            print(f'{prefix_file}{bcolors.WARNING}{filename} ✓{bcolors.ENDC}')
+        elif tidy > 1:
+            print(f'{prefix_file}{bcolors.FAIL}{filename} ✘{bcolors.ENDC}')
+
+    for dirname in [os.path.basename(path) for path in dirs]:
+        print(prefix + '├── ' + dirname)
+        tidy_dir(os.path.join(path, dirname), level=level+1, prefix=prefix + '│   ', **kwargs)
 
 
 def main():
@@ -19,9 +85,7 @@ def main():
     parser.add_argument('a')
     args = parser.parse_args()
 
-    # TODO tidy json files?
-
-    # TODO tidy html files
+    tidy_dir(args.a)
 
     return 0
 
