2b5518e33e95e2c01f69fb90fcb21be2138e16af
Andreas Stefl <astefl@esri.com>
2021-03-12 13:04:50 +0100

    tmp

--- src/internal/abstract/document2.h
+++ src/internal/abstract/document2.h
@@ -34,6 +34,7 @@ struct element_identifier_tag {};
 
 using ElementIdentifier = Identifier<std::uint64_t, element_identifier_tag>;
 
+// TODO it might be possible to strongly type these properties
 enum class DocumentElementProperty {
   NAME,
   NOTES,
@@ -97,58 +98,126 @@ class Document2 {
 public:
   virtual ~Document2() = default;
 
+  /// \return `true` if the document is editable in any way.
   [[nodiscard]] virtual bool editable() const noexcept = 0;
+
+  /// \param encrypted to ask for encrypted saves.
+  /// \return `true` if the document is savable.
   [[nodiscard]] virtual bool savable(bool encrypted) const noexcept = 0;
 
+  /// \param path the destination path.
   virtual void save(const common::Path &path) const = 0;
-  virtual void save(const common::Path &path,
-                    const std::string &password) const = 0;
 
+  /// \param path the destination path.
+  /// \param password the encryption password.
+  virtual void save(const common::Path &path, const char *password) const = 0;
+
+  /// \return the type of the document.
   [[nodiscard]] virtual experimental::DocumentType
   document_type() const noexcept = 0;
+
+  /// \return the metadata of the document.
   [[nodiscard]] virtual experimental::DocumentMeta
   document_meta() const noexcept = 0;
+
+  /// \return the amount of entities in the document
+  ///         (e.g. slides, sheets and pages).
   [[nodiscard]] virtual std::uint32_t entry_count() const = 0;
+
+  /// \return the root element of the document.
   [[nodiscard]] virtual ElementIdentifier root_element() const = 0;
 
+  /// \param elementId the element to query.
+  /// \return the type of the element.
   [[nodiscard]] virtual experimental::ElementType
   element_type(ElementIdentifier elementId) const = 0;
 
+  /// \param elementId the element to query.
+  /// \return the parent of the element.
   [[nodiscard]] virtual ElementIdentifier
   element_parent(ElementIdentifier elementId) const = 0;
+
+  /// \param elementId the element to query.
+  /// \return the first child of the element.
   [[nodiscard]] virtual ElementIdentifier
   element_first_child(ElementIdentifier elementId) const = 0;
+
+  /// \param elementId the element to query.
+  /// \return the previous sibling of the element.
   [[nodiscard]] virtual ElementIdentifier
   element_previous_sibling(ElementIdentifier elementId) const = 0;
+
+  /// \param elementId the element to query.
+  /// \return the next sibling of the element.
   [[nodiscard]] virtual ElementIdentifier
   element_next_sibling(ElementIdentifier elementId) const = 0;
 
+  /// \param elementId the element to query.
+  /// \param property the requested property.
+  /// \return the requested optional value.
   [[nodiscard]] virtual std::any
   element_property(ElementIdentifier elementId,
                    DocumentElementProperty property) const = 0;
-  [[nodiscard]] virtual std::string
+
+  /// \param elementId the element to query.
+  /// \param property the requested property.
+  /// \return the requested optional string.
+  ///         - return value must not be null
+  ///         - lifetime of the return value only guaranteed until next
+  ///           function call on the interface.
+  [[nodiscard]] virtual const char *
   element_string_property(ElementIdentifier elementId,
                           DocumentElementProperty property) const = 0;
+
+  /// \param elementId the element to query.
+  /// \param property the requested property.
+  /// \return the requested integer.
   [[nodiscard]] virtual std::uint32_t
   element_uint32_property(ElementIdentifier elementId,
                           DocumentElementProperty property) const = 0;
+
+  /// \param elementId the element to query.
+  /// \param property the requested property.
+  /// \return the requested bool.
   [[nodiscard]] virtual bool
   element_bool_property(ElementIdentifier elementId,
                         DocumentElementProperty property) const = 0;
-  [[nodiscard]] virtual std::optional<std::string>
+
+  /// \param elementId the element to query.
+  /// \param property the requested property.
+  /// \return the requested optional string.
+  ///         - return value might be null
+  ///         - lifetime of the return value only guaranteed until next
+  ///           function call on the interface.
+  [[nodiscard]] virtual const char *
   element_optional_string_property(ElementIdentifier elementId,
                                    DocumentElementProperty property) const = 0;
 
+  /// \param elementId the element to query.
+  /// \return the requested file object.
+  ///         - return value should not be null
+  ///         - in case of external images it might be null for now.
   [[nodiscard]] virtual std::shared_ptr<File>
   image_file(ElementIdentifier elementId) const = 0;
 
+  /// \param elementId the element.
+  /// \param property the property to set.
+  /// \param value the value to set.
   virtual void set_element_property(ElementIdentifier elementId,
                                     DocumentElementProperty property,
                                     const std::any &value) const = 0;
+
+  /// \param elementId the element.
+  /// \param property the property to set.
+  /// \param value the value to set.
+  ///        - lifetime of `value` must be guaranteed until the function
+  ///          completes.
   virtual void set_element_string_property(ElementIdentifier elementId,
                                            DocumentElementProperty property,
-                                           const std::string &value) const = 0;
+                                           const char *value) const = 0;
 
+  /// \param elementId the element.
+  /// \param property the property to remove.
   virtual void
   remove_element_property(ElementIdentifier elementId,
                           DocumentElementProperty property) const = 0;
