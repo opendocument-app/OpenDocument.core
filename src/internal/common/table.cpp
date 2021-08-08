41bfbefb6d4c95d6a8483dad067c0f11863409da
Andreas Stefl <astefl@esri.com>
2021-08-08 14:27:44 +0200

    tmp

--- src/internal/common/table.cpp
+++ src/internal/common/table.cpp
@@ -7,27 +7,28 @@ TableDimensions Table::dimensions() const { return m_dimensions; }
 
 common::TableRange Table::content_bounds() const { return m_content_bounds; }
 
-ElementIdentifier Table::cell_first_child(const std::uint32_t row,
-                                          const std::uint32_t column) const {
+ElementIdentifier
+Table::cell_first_child(const std::uint32_t /*row*/,
+                        const std::uint32_t /*column*/) const {
   // TODO
   return {};
 }
 
-TableDimensions Table::cell_span(const std::uint32_t row,
-                                 const std::uint32_t column) const {
+TableDimensions Table::cell_span(const std::uint32_t /*row*/,
+                                 const std::uint32_t /*column*/) const {
   // TODO
   return {};
 }
 
 std::unordered_map<ElementProperty, std::any>
-Table::properties(std::uint32_t row, std::uint32_t column) const {
+Table::properties(std::uint32_t /*row*/, std::uint32_t /*column*/) const {
   // TODO
   return {};
 }
 
 void Table::update_properties(
-    const std::uint32_t row, const std::uint32_t column,
-    std::unordered_map<ElementProperty, std::any> properties) const {
+    const std::uint32_t /*row*/, const std::uint32_t /*column*/,
+    std::unordered_map<ElementProperty, std::any> /*properties*/) const {
   // TODO
 }
 
