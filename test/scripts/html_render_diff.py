26594a5b39c1e4810fad99092f7ba35cf86dc5bf
Andreas Stefl <astefl@esri.com>
2021-09-14 12:01:05 +0200

    improve test scripts

--- test/scripts/html_render_diff.py
+++ test/scripts/html_render_diff.py
@@ -57,9 +57,10 @@ def main():
     if bounding_box:
         print('images are different')
         print('first error at %f%%' % (1e2 * bounding_box[1] / diff.height))
-        print('bounding box %f%%' % (
-                1e2 * ((bounding_box[2] - bounding_box[0]) * (bounding_box[3] - bounding_box[1])) / (
-                diff.width * diff.height)))
+        print('bounding box %f%%' %
+              (1e2 * ((bounding_box[2] - bounding_box[0]) *
+                      (bounding_box[3] - bounding_box[1])) /
+               (diff.width * diff.height)))
         return 1
 
     print('images are the same')
