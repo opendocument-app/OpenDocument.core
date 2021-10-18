f9fb5a0568a2404540c75a6ad492eabced2a2fad
andiwand <stefl.andreas@gmail.com>
2021-10-18 10:18:50 +0200

    tmp

--- src/open_document_reader.cpp
+++ src/open_document_reader.cpp
@@ -15,9 +15,8 @@ std::string OpenDocumentReader::commit() noexcept {
   return internal::common::constants::commit();
 }
 
-std::optional<Document> OpenDocumentReader::html(const std::string &path,
-                                                 const char *password,
-                                                 const HtmlConfig &config) {
+Html OpenDocumentReader::html(const std::string &path, const char *password,
+                              const HtmlConfig &config) {
   DecodedFile file(path);
 
   if (file.file_category() == FileCategory::document) {
@@ -29,20 +28,43 @@ std::optional<Document> OpenDocumentReader::html(const std::string &path,
     }
     auto document = document_file.document();
 
+    std::vector<HtmlPage> pages;
+
     html::translate(document, path, config);
 
-    return document;
+    return Html(file.file_type(), config, std::move(pages),
+                std::move(document));
   }
 
   throw UnknownFileType();
 }
 
-void OpenDocumentReader::edit(Document &document, const char *diff,
-                              const std::string &path) {
-  html::edit(document, diff);
-  document.save(path);
+OpenDocumentReader::OpenDocumentReader() = default;
+
+Html::Html(FileType file_type, HtmlConfig config, std::vector<HtmlPage> pages,
+           Document document)
+    : m_file_type{file_type}, m_config{std::move(config)},
+      m_pages{std::move(pages)}, m_document{std::move(document)} {}
+
+const std::vector<HtmlPage> &Html::pages() const { return m_pages; }
+
+void Html::edit(const char *diff) {
+  if (m_document) {
+    html::edit(*m_document, diff);
+  }
 }
 
-OpenDocumentReader::OpenDocumentReader() = default;
+void Html::save(const std::string &path) const {
+  if (m_document) {
+    m_document->save(path);
+  }
+}
+
+HtmlPage::HtmlPage(std::string name, std::string path)
+    : m_name{std::move(name)}, m_path{std::move(path)} {}
+
+const std::string &HtmlPage::name() const { return m_name; }
+
+const std::string &HtmlPage::path() const { return m_path; }
 
 } // namespace odr
