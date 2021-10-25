594f00ff7b75fdb0e0f7bbc4b2f2d0337b6f7856
andiwand <stefl.andreas@gmail.com>
2021-10-25 10:46:21 +0200

    tmp

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,8 @@ from conans import ConanFile, CMake
 
 class OpenDocumentCoreConan(ConanFile):
     settings = "os", "compiler", "cppstd", "build_type", "arch"
-    requires = "pugixml/1.11", "cryptopp/8.5.0", "miniz/2.1.0", "nlohmann_json/3.10.2", "vincentlaucsb-csv-parser/2.1.3", "gtest/1.11.0", "glog/0.5.0"
+    requires = "pugixml/1.11", "cryptopp/8.5.0", "miniz/2.1.0", "nlohmann_json/3.10.2",\
+               "vincentlaucsb-csv-parser/2.1.3", "gtest/1.11.0", "glog/0.4.0"
     generators = "cmake", "gcc", "txt"
     default_options = {}
 
