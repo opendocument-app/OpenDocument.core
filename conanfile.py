875bc81ef7a6a61e73d78c24dd64af26ff8390fe
andiwand <stefl.andreas@gmail.com>
2021-11-18 09:46:34 +0100

    tmp

--- conanfile.py
+++ conanfile.py
@@ -2,11 +2,26 @@ from conans import ConanFile, CMake
 
 
 class OpenDocumentCoreConan(ConanFile):
+    name = "odr.core"
+    url = ""
+    homepage = "https://github.com/opendocument-app/OpenDocument.core"
+    description = "C++ library that translates office documents to HTML"
+    topics = "open document", "openoffice xml", "open document reader"
+    license = "GPL 3.0"
+
     settings = "os", "compiler", "cppstd", "build_type", "arch"
-    requires = "pugixml/1.11", "cryptopp/8.5.0", "miniz/2.1.0", "nlohmann_json/3.10.4",\
-               "vincentlaucsb-csv-parser/2.1.3", "gtest/1.11.0", "glog/0.5.0"
-    generators = "cmake", "gcc", "txt"
-    default_options = {}
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    requires = "pugixml/1.11", "cryptopp/8.5.0", "miniz/2.1.0", "nlohmann_json/3.10.4", \
+               "vincentlaucsb-csv-parser/2.1.3", "gtest/1.11.0"
+    generators = "cmake", "cmake_find_package_multi", "gcc", "txt"
 
     def build(self):
         cmake = CMake(self)
