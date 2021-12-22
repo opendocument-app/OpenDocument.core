f3a7c3a2f9638ebd6d30bdd88b3e10f84c41f834
Andreas Stefl <stefl.andreas@gmail.com>
2021-12-22 14:50:53 +0100

    refactor 3rdparty cmake (#295)

---
+++ 3rdparty/csv.cmake
@@ -0,0 +1,13 @@
+FetchContent_Declare(
+        csv-parser
+        GIT_REPOSITORY https://github.com/andiwand/csv-parser.git
+        GIT_TAG fix/cmake
+)
+
+FetchContent_GetProperties(csv-parser)
+if (NOT csv-parser_POPULATED)
+    FetchContent_Populate(csv-parser)
+    set(CSV_DEVELOPER FALSE CACHE BOOL "" FORCE)
+    add_subdirectory(${csv-parser_SOURCE_DIR} ${csv-parser_BINARY_DIR} EXCLUDE_FROM_ALL)
+    target_include_directories(csv PUBLIC "${csv-parser_SOURCE_DIR}/include")
+endif ()
