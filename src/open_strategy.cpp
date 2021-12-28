b8eb6cdb127509a99b1cee7bdca6368a382d52d1
andiwand <stefl.andreas@gmail.com>
2021-12-28 12:38:36 +0100

    tmp

--- src/open_strategy.cpp
+++ src/open_strategy.cpp
@@ -2,11 +2,13 @@
 #include <internal/cfb/cfb_archive.h>
 #include <internal/common/archive.h>
 #include <internal/common/file.h>
+#include <internal/common/image_file.h>
 #include <internal/csv/csv_file.h>
 #include <internal/json/json_file.h>
 #include <internal/odf/odf_file.h>
 #include <internal/oldms/oldms_file.h>
 #include <internal/ooxml/ooxml_file.h>
+#include <internal/svm/svm_file.h>
 #include <internal/text/text_file.h>
 #include <internal/zip/zip_archive.h>
 #include <magic.h>
@@ -25,10 +27,10 @@ open_strategy::types(std::shared_ptr<abstract::File> file) {
 
   auto file_type = magic::file_type(*file);
 
-  if (file_type == FileType::zip) {
-    // TODO if `file` is in memory we would copy it unnecessarily
-    auto memory_file = std::make_shared<common::MemoryFile>(*file);
+  // TODO if `file` is in memory we would copy it unnecessarily
+  auto memory_file = std::make_shared<common::MemoryFile>(*file);
 
+  if (file_type == FileType::zip) {
     auto zip = common::ArchiveFile(zip::ReadonlyZipArchive(memory_file));
     result.push_back(FileType::zip);
 
@@ -44,9 +46,6 @@ open_strategy::types(std::shared_ptr<abstract::File> file) {
     } catch (...) {
     }
   } else if (file_type == FileType::compound_file_binary_format) {
-    // TODO if `file` is in memory we would copy it unnecessarily
-    auto memory_file = std::make_shared<common::MemoryFile>(*file);
-
     auto cfb = common::ArchiveFile(cfb::ReadonlyCfbArchive(memory_file));
     result.push_back(FileType::compound_file_binary_format);
 
@@ -59,6 +58,16 @@ open_strategy::types(std::shared_ptr<abstract::File> file) {
 
     try {
       result.push_back(ooxml::OfficeOpenXmlFile(filesystem).file_type());
+    } catch (...) {
+    }
+  } else if (file_type == FileType::portable_network_graphics ||
+             file_type == FileType::graphics_interchange_format ||
+             file_type == FileType::jpeg ||
+             file_type == FileType::bitmap_image_file) {
+    result.push_back(file_type);
+  } else if (file_type == FileType::starview_metafile) {
+    try {
+      result.push_back(svm::SvmFile(memory_file).file_type());
     } catch (...) {
     }
   }
@@ -86,10 +95,10 @@ std::unique_ptr<abstract::DecodedFile>
 open_strategy::open_file(std::shared_ptr<abstract::File> file) {
   auto file_type = magic::file_type(*file);
 
-  if (file_type == FileType::zip) {
-    // TODO if `file` is in memory we would copy it unnecessarily
-    auto memory_file = std::make_shared<common::MemoryFile>(*file);
+  // TODO if `file` is in memory we would copy it unnecessarily
+  auto memory_file = std::make_shared<common::MemoryFile>(*file);
 
+  if (file_type == FileType::zip) {
     auto zip = std::make_unique<common::ArchiveFile<zip::ReadonlyZipArchive>>(
         zip::ReadonlyZipArchive(memory_file));
 
@@ -107,9 +116,6 @@ open_strategy::open_file(std::shared_ptr<abstract::File> file) {
 
     return zip;
   } else if (file_type == FileType::compound_file_binary_format) {
-    // TODO if `file` is in memory we would copy it unnecessarily
-    auto memory_file = std::make_unique<common::MemoryFile>(*file);
-
     auto cfb = std::make_unique<common::ArchiveFile<cfb::ReadonlyCfbArchive>>(
         cfb::ReadonlyCfbArchive(std::move(memory_file)));
 
@@ -126,8 +132,14 @@ open_strategy::open_file(std::shared_ptr<abstract::File> file) {
     }
 
     return cfb;
+  } else if (file_type == FileType::portable_network_graphics ||
+             file_type == FileType::graphics_interchange_format ||
+             file_type == FileType::jpeg ||
+             file_type == FileType::bitmap_image_file) {
+    return std::make_unique<common::ImageFile>(file, file_type);
+  } else if (file_type == FileType::starview_metafile) {
+    return std::make_unique<svm::SvmFile>(memory_file);
   } else if (file_type == FileType::unknown) {
-
     try {
       auto text = std::make_shared<text::TextFile>(file);
 
