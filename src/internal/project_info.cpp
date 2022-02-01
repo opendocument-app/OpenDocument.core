d8e37f9d61370a597db990d5570d8ae438f3723e
andiwand <stefl.andreas@gmail.com>
2022-02-01 12:11:45 +0100

    tmp

---
+++ src/internal/project_info.cpp
@@ -0,0 +1,10 @@
+#include <internal/project_info.h>
+
+namespace odr::internal {
+
+const char *project_info::version() noexcept {
+  // TODO fill if version present
+  return "";
+}
+
+} // namespace odr::internal
