e771858bce9fec7a52a3c1c8f1ea156d6bd06068
andiwand <stefl.andreas@gmail.com>
2022-06-26 21:23:22 +0200

    fix build

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,8 @@ class OpenDocumentCoreConan(ConanFile):
     exports_sources = ["cli/*", "cmake/*", "include/*", "src/*", "CMakeLists.txt"]
 
     requires = ["pugixml/1.11", "cryptopp/8.5.0", "miniz/2.1.0", "nlohmann_json/3.10.4",
-                "vincentlaucsb-csv-parser/2.1.3", "uchardet/0.0.7", "pdf2htmlEX/0.18.8-rc1"]
+                "vincentlaucsb-csv-parser/2.1.3", "uchardet/0.0.7", "pdf2htmlEX/0.18.8-rc1",
+                "poppler/0.89.0"]
     build_requires = ["gtest/1.11.0"]
     generators = "cmake_paths", "cmake_find_package"
 
