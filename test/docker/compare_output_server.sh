1557fb717848e1f65e41932f69d8aac54619ed48
Andreas Stefl <stefl.andreas@gmail.com>
2023-11-26 21:54:43 +0100

    use docker image from ghcr (#336)

--- test/docker/compare_output_server.sh
+++ test/docker/compare_output_server.sh
@@ -4,5 +4,11 @@ REF="test/data/reference-output/"
 OBS="cmake-build-debug/test/output/"
 DRIVER="firefox"
 
-docker build --tag odr_core_test test/docker
-docker run -ti -v $(pwd):/repo -p 5000:5000 odr_core_test python3  /repo/test/scripts/compare_output_server.py /repo/$REF /repo/$OBS --compare --driver $DRIVER
+# manually build the image
+#docker build --tag odr_core_test test/docker
+
+docker run -ti \
+  -v $(pwd):/repo \
+  -p 5000:5000 \
+  ghcr.io/opendocument-app/odr_core_test:sha-f9aab98 \
+  python3 /repo/test/scripts/compare_output_server.py /repo/$REF /repo/$OBS --compare --driver $DRIVER
