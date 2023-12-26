7708abefa1c4c03a7e70f956db1fd3d09933b2e2
andiwand <stefl.andreas@gmail.com>
2023-12-26 15:47:45 +0100

    revert text file changes

--- src/odr/internal/html/text_file.cpp
+++ src/odr/internal/html/text_file.cpp
@@ -16,7 +16,63 @@ namespace odr::internal {
 Html html::translate_text_file(const TextFile &text_file,
                                const std::string &path,
                                const HtmlConfig &config) {
-  return Html(FileType::text_file, config, {});
+  auto output_path = path + "/text.html";
+  std::ofstream ostream(output_path);
+  if (!ostream.is_open()) {
+    throw FileWriteError();
+  }
+  auto in = text_file.stream();
+  HtmlWriter out(ostream, config.format_html, config.html_indent);
+
+  out.write_begin();
+
+  out.write_header_begin();
+  out.write_header_charset("UTF-8");
+  out.write_header_target("_blank");
+  out.write_header_title("odr");
+  out.write_header_viewport(
+      "width=device-width,initial-scale=1.0,user-scalable=yes");
+  out.write_header_style_begin();
+  out.out() << "*{font-family:monospace;}";
+  out.out() << "td{padding-left:10px;padding-right:10px;}";
+  out.write_header_style_end();
+  out.write_header_end();
+
+  out.write_body_begin();
+  out.write_element_begin(
+      "table",
+      {.attributes = HtmlAttributesVector{
+           {"cellpadding", "0"}, {"border", "0"}, {"cellspacing", "0"}}});
+
+  for (std::uint32_t line = 1;; ++line) {
+    out.write_element_begin("tr");
+
+    out.write_element_begin("td",
+                            {.inline_element = true,
+                             .style = "text-align:right;user-select:none;"});
+    out.out() << line;
+    out.write_element_end("td");
+
+    out.write_element_begin("td");
+
+    std::ostringstream ss_out;
+    util::stream::getline(*in, ss_out);
+    out.out() << escape_text(ss_out.str());
+
+    out.write_element_end("td");
+    out.write_element_end("tr");
+
+    if (in->eof()) {
+      break;
+    }
+  }
+
+  out.write_element_end("table");
+  out.write_body_end();
+
+  out.write_end();
+
+  return {text_file.file_type(), config, {{"text", output_path}}};
 }
 
 } // namespace odr::internal
