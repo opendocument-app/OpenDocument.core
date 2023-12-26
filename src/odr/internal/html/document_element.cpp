695ab0e4997d3ad6f43801351f4672776ae4cf09
andiwand <stefl.andreas@gmail.com>
2023-12-26 16:37:28 +0100

    more backports

--- src/odr/internal/html/document_element.cpp
+++ src/odr/internal/html/document_element.cpp
@@ -278,9 +278,9 @@ void html::translate_paragraph(Element element, HtmlWriter &out,
 
     // TODO example `style-missing+image-1.odt` first paragraph has no height
   } else {
-    out.write_element_begin(
-        "x-s", {.inline_element = true,
-                .style = translate_text_style(paragraph.text_style())});
+    out.write_element_begin("x-s",
+                            HtmlElementOptions().set_inline(true).set_style(
+                                translate_text_style(paragraph.text_style())));
     out.write_element_end("x-s");
   }
   out.write_element_begin("wbr", {.close_type = HtmlCloseType::none});
@@ -291,8 +291,9 @@ void html::translate_span(Element element, HtmlWriter &out,
                           const HtmlConfig &config) {
   Span span = element.span();
 
-  out.write_element_begin("x-s", {.inline_element = true,
-                                  .style = translate_text_style(span.style())});
+  out.write_element_begin("x-s",
+                          HtmlElementOptions().set_inline(true).set_style(
+                              translate_text_style(span.style())));
   translate_children(span.children(), out, config);
   out.write_element_end("x-s");
 }
@@ -301,9 +302,9 @@ void html::translate_link(Element element, HtmlWriter &out,
                           const HtmlConfig &config) {
   Link link = element.link();
 
-  out.write_element_begin(
-      "a", {.inline_element = true,
-            .attributes = HtmlAttributesVector{{"href", link.href()}}});
+  out.write_element_begin("a",
+                          HtmlElementOptions().set_inline(true).set_attributes(
+                              HtmlAttributesVector{{"href", link.href()}}));
   translate_children(link.children(), out, config);
   out.write_element_end("a");
 }
@@ -312,9 +313,9 @@ void html::translate_bookmark(Element element, HtmlWriter &out,
                               const HtmlConfig & /*config*/) {
   Bookmark bookmark = element.bookmark();
 
-  out.write_element_begin(
-      "a", {.inline_element = true,
-            .attributes = HtmlAttributesVector{{"id", bookmark.name()}}});
+  out.write_element_begin("a",
+                          HtmlElementOptions().set_inline(true).set_attributes(
+                              HtmlAttributesVector{{"id", bookmark.name()}}));
   out.write_element_end("a");
 }
 
@@ -329,8 +330,8 @@ void html::translate_list_item(Element element, HtmlWriter &out,
                                const HtmlConfig &config) {
   ListItem list_item = element.list_item();
 
-  out.write_element_begin("li",
-                          {.style = translate_text_style(list_item.style())});
+  out.write_element_begin("li", HtmlElementOptions().set_style(
+                                    translate_text_style(list_item.style())));
   translate_children(list_item.children(), out, config);
   out.write_element_end("li");
 }
@@ -340,24 +341,27 @@ void html::translate_table(Element element, HtmlWriter &out,
   Table table = element.table();
 
   out.write_element_begin(
-      "table", {.attributes = HtmlAttributesVector{{"cellpadding", "0"},
-                                                   {"border", "0"},
-                                                   {"cellspacing", "0"}},
-                .style = translate_table_style(table.style())});
+      "table",
+      HtmlElementOptions()
+          .set_attributes(HtmlAttributesVector{
+              {"cellpadding", "0"}, {"border", "0"}, {"cellspacing", "0"}})
+          .set_style(translate_table_style(table.style())));
 
   for (Element column : table.columns()) {
     TableColumn table_column = column.table_column();
 
-    out.write_element_begin(
-        "col", {.close_type = HtmlCloseType::none,
-                .style = translate_table_column_style(table_column.style())});
+    out.write_element_begin("col", HtmlElementOptions()
+                                       .set_close_type(HtmlCloseType::none)
+                                       .set_style(translate_table_column_style(
+                                           table_column.style())));
   }
 
   for (Element row : table.rows()) {
     TableRow table_row = row.table_row();
 
-    out.write_element_begin(
-        "tr", {.style = translate_table_row_style(table_row.style())});
+    out.write_element_begin("tr",
+                            HtmlElementOptions().set_style(
+                                translate_table_row_style(table_row.style())));
 
     for (Element cell : table_row.children()) {
       TableCell table_cell = cell.table_cell();
@@ -366,16 +370,17 @@ void html::translate_table(Element element, HtmlWriter &out,
         TableDimensions cell_span = table_cell.span();
 
         out.write_element_begin(
-            "td", {.attributes =
-                       [&](const HtmlAttributeWriterCallback &clb) {
-                         if (cell_span.columns > 1) {
-                           clb("colspan", std::to_string(cell_span.columns));
-                         }
-                         if (cell_span.rows > 1) {
-                           clb("rowspan", std::to_string(cell_span.rows));
-                         }
-                       },
-                   .style = translate_table_cell_style(table_cell.style())});
+            "td",
+            HtmlElementOptions()
+                .set_attributes([&](const HtmlAttributeWriterCallback &clb) {
+                  if (cell_span.columns > 1) {
+                    clb("colspan", std::to_string(cell_span.columns));
+                  }
+                  if (cell_span.rows > 1) {
+                    clb("rowspan", std::to_string(cell_span.rows));
+                  }
+                })
+                .set_style(translate_table_cell_style(table_cell.style())));
 
         translate_children(cell.children(), out, config);
 
@@ -395,19 +400,19 @@ void html::translate_image(Element element, HtmlWriter &out,
 
   out.write_element_begin(
       "img",
-      {.close_type = HtmlCloseType::trailing,
-       .attributes =
-           [&](const HtmlAttributeWriterCallback &clb) {
-             clb("alt", "Error: image not found or unsupported");
-             if (image.is_internal()) {
-               clb("src", [&](std::ostream &o) {
-                 translate_image_src(image.file().value(), o, config);
-               });
-             } else {
-               clb("src", image.href());
-             }
-           },
-       .style = "position:absolute;left:0;top:0;width:100%;height:100%"});
+      HtmlElementOptions()
+          .set_close_type(HtmlCloseType::trailing)
+          .set_attributes([&](const HtmlAttributeWriterCallback &clb) {
+            clb("alt", "Error: image not found or unsupported");
+            if (image.is_internal()) {
+              clb("src", [&](std::ostream &o) {
+                translate_image_src(image.file().value(), o, config);
+              });
+            } else {
+              clb("src", image.href());
+            }
+          })
+          .set_style("position:absolute;left:0;top:0;width:100%;height:100%"));
 }
 
 void html::translate_frame(Element element, HtmlWriter &out,
@@ -415,8 +420,9 @@ void html::translate_frame(Element element, HtmlWriter &out,
   Frame frame = element.frame();
   GraphicStyle style = frame.style();
 
-  out.write_element_begin("div", {.style = translate_frame_properties(frame) +
-                                           translate_drawing_style(style)});
+  out.write_element_begin(
+      "div", HtmlElementOptions().set_style(translate_frame_properties(frame) +
+                                            translate_drawing_style(style)));
   translate_children(frame.children(), out, config);
   out.write_element_end("div");
 }
@@ -426,8 +432,9 @@ void html::translate_rect(Element element, HtmlWriter &out,
   Rect rect = element.rect();
   GraphicStyle style = rect.style();
 
-  out.write_element_begin("div", {.style = translate_rect_properties(rect) +
-                                           translate_drawing_style(style)});
+  out.write_element_begin(
+      "div", HtmlElementOptions().set_style(translate_rect_properties(rect) +
+                                            translate_drawing_style(style)));
   translate_children(rect.children(), out, config);
   out.write_new_line();
   out.write_raw(
@@ -441,19 +448,21 @@ void html::translate_line(Element element, HtmlWriter &out,
   GraphicStyle style = line.style();
 
   out.write_element_begin(
-      "svg", {.attributes =
-                  HtmlAttributesVector{{"xmlns", "http://www.w3.org/2000/svg"},
-                                       {"version", "1.1"},
-                                       {"overflow", "visible"}},
-              .style = "z-index:-1;position:absolute;top:0;left:0;" +
-                       translate_drawing_style(style)});
+      "svg", HtmlElementOptions()
+                 .set_attributes(HtmlAttributesVector{
+                     {"xmlns", "http://www.w3.org/2000/svg"},
+                     {"version", "1.1"},
+                     {"overflow", "visible"}})
+                 .set_style("z-index:-1;position:absolute;top:0;left:0;" +
+                            translate_drawing_style(style)));
 
   out.write_element_begin(
-      "line", {.close_type = HtmlCloseType::trailing,
-               .attributes = HtmlAttributesVector{{"x1", line.x1()},
-                                                  {"y1", line.y1()},
-                                                  {"x2", line.x2()},
-                                                  {"y2", line.y2()}}});
+      "line", HtmlElementOptions()
+                  .set_close_type(HtmlCloseType::trailing)
+                  .set_attributes(HtmlAttributesVector{{"x1", line.x1()},
+                                                       {"y1", line.y1()},
+                                                       {"x2", line.x2()},
+                                                       {"y2", line.y2()}}));
 
   out.write_element_end("svg");
 }
@@ -463,8 +472,9 @@ void html::translate_circle(Element element, HtmlWriter &out,
   Circle circle = element.circle();
   GraphicStyle style = circle.style();
 
-  out.write_element_begin("div", {.style = translate_circle_properties(circle) +
-                                           translate_drawing_style(style)});
+  out.write_element_begin("div", HtmlElementOptions().set_style(
+                                     translate_circle_properties(circle) +
+                                     translate_drawing_style(style)));
   out.write_new_line();
   translate_children(circle.children(), out, config);
   out.write_raw(
@@ -477,9 +487,10 @@ void html::translate_custom_shape(Element element, HtmlWriter &out,
   CustomShape custom_shape = element.custom_shape();
   GraphicStyle style = custom_shape.style();
 
-  out.write_element_begin(
-      "div", {.style = translate_custom_shape_properties(custom_shape) +
-                       translate_drawing_style(style)});
+  out.write_element_begin("div",
+                          HtmlElementOptions().set_style(
+                              translate_custom_shape_properties(custom_shape) +
+                              translate_drawing_style(style)));
   translate_children(custom_shape.children(), out, config);
   // TODO draw shape in svg
   out.write_element_end("div");
