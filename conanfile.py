eb640215688325a56bf2a7199aaeb1655f213daa
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-13 10:09:30 +0300

    Fix the mess up, created when trying to use the same conanfile in here and the conan-odr-index

--- conanfile.py
+++ conanfile.py
@@ -27,17 +27,7 @@ class OpenDocumentCoreConan(ConanFile):
         "fPIC": True,
     }
 
-    def export_sources(self):
-        if not self.conan_data is None:
-            for src in ["cli/*", "cmake/*", "src/*", "CMakeLists.txt"]:
-                copy(self, src, self.recipe_folder, self.export_sources_folder)
-        else:
-            export_conandata_patches(self)
-
-    def source(self):
-        if not self.conan_data is None:
-            get(self, **self.conan_data["sources"][self.version]["source"], strip_root=True)
-            apply_conandata_patches(self)
+    export_sources = ["cli/*", "cmake/*", "src/*", "CMakeLists.txt"]
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -47,13 +37,7 @@ class OpenDocumentCoreConan(ConanFile):
         if self.options.shared:
             self.options.rm_safe("fPIC")
 
-    def layout(self):
-        cmake_layout(self, src_folder="src")
-
     def requirements(self):
-        if Version(self.version) <= "2.0.0" and Version(self.version) != "0.0.0":
-            return
-
         self.requires("pugixml/1.14")
         self.requires("cryptopp/8.8.0")
         self.requires("miniz/3.0.2")
@@ -63,26 +47,16 @@ class OpenDocumentCoreConan(ConanFile):
         self.requires("utfcpp/4.0.4")
 
     def build_requirements(self):
-        if Version(self.version) <= "2.0.0" and Version(self.version) != "0.0.0":
-            return
-
         self.test_requires("gtest/1.14.0")
 
     def validate_build(self):
         if self.settings.get_safe("compiler.cppstd"):
-            if Version(self.version) >= "4.0.0" or Version(self.version) == "0.0.0":
-                check_min_cppstd(self, 20)
-            elif Version(self.version) >= "2.0.0":
-                check_min_cppstd(self, 17)
-            else:
-                check_min_cppstd(self, 14)
+            check_min_cppstd(self, 20)
 
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_PROJECT_VERSION"] = self.version
         tc.variables["ODR_TEST"] = False
-        if Version(self.version) <= "4.0.0" and Version(self.version) != "0.0.0":
-            tc.variables["CONAN_EXPORTED"] = True
         tc.generate()
 
         deps = CMakeDeps(self)
@@ -105,9 +79,4 @@ class OpenDocumentCoreConan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        if Version(self.version) >= "2.1.0" or Version(self.version) == "0.0.0":
-            self.cpp_info.libs = ["odr"]
-        elif Version(self.version) == "2.0.0":
-            self.cpp_info.libs = ["odr-static"]
-        else:
-            self.cpp_info.libs = ["odrlib"]
+        self.cpp_info.libs = ["odr"]
