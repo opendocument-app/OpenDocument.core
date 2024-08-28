fa7803a5ea70f7087402720ec5b49eed9a2fb8bb
Vilius Sutkus '89 <Vilius@ViliusSutkus89.com>
2024-08-28 22:35:39 +0300

    Format pdf2htmlEX_wrapper_test

--- test/src/pdf2htmlEX_wrapper_test.cpp
+++ test/src/pdf2htmlEX_wrapper_test.cpp
@@ -3,12 +3,12 @@
 #include <optional>
 #include <string>
 
-#include <test_util.hpp>
 #include <gtest/gtest.h>
+#include <test_util.hpp>
 
-#include <odr/internal/html/pdf2htmlEX_wrapper.hpp>
 #include <odr/html.hpp>
 #include <odr/internal/common/path.hpp>
+#include <odr/internal/html/pdf2htmlEX_wrapper.hpp>
 #include <odr/internal/util/string_util.hpp>
 
 using namespace odr;
@@ -24,8 +24,11 @@ TEST_P(pdf2htmlEXWrapperTests, html) {
   const TestFile test_file = TestData::test_file(test_file_path);
 
   const std::string test_repo = *common::Path(test_file_path).begin();
-  const std::string output_path_prefix =
-      common::Path("output").join(test_repo).join("output").join("pdf2htmlEX").string();
+  const std::string output_path_prefix = common::Path("output")
+                                             .join(test_repo)
+                                             .join("output")
+                                             .join("pdf2htmlEX")
+                                             .string();
   const std::string output_path =
       common::Path(output_path_prefix)
           .join(common::Path(test_file_path).rebase(test_repo))
@@ -33,14 +36,16 @@ TEST_P(pdf2htmlEXWrapperTests, html) {
 
   std::cout << test_file.path << " to " << output_path << std::endl;
 
-  if (!util::string::ends_with(test_file.path, ".pdf") && test_file.type != FileType::portable_document_format) {
+  if (!util::string::ends_with(test_file.path, ".pdf") &&
+      test_file.type != FileType::portable_document_format) {
     GTEST_SKIP();
   }
 
   fs::create_directories(output_path);
   HtmlConfig config;
   std::optional<std::string> password;
-  Html html = odr::internal::html::pdf2htmlEX_wrapper(test_file.path, output_path, config, password);
+  Html html = odr::internal::html::pdf2htmlEX_wrapper(
+      test_file.path, output_path, config, password);
 
   for (const HtmlPage &html_page : html.pages()) {
     EXPECT_TRUE(fs::is_regular_file(html_page.path));
