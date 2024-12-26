0dec429d51a1b7b0af693f25cb44072e8ec4c858
Andreas Stefl <stefl.andreas@gmail.com>
2024-12-26 15:40:52 +0100

    update conan deps versions

--- conanfile.py
+++ conanfile.py
@@ -44,9 +44,9 @@ class OpenDocumentCoreConan(ConanFile):
         self.requires("uchardet/0.0.8")
         self.requires("utfcpp/4.0.4")
         if self.options.get_safe("with_pdf2htmlEX", False):
-            self.requires("pdf2htmlex/0.18.8.rc1-git-6f85c88")
+            self.requires("pdf2htmlex/0.18.8.rc1-20240905-git-vili")
         if self.options.get_safe("with_wvWare", False):
-            self.requires("wvware/1.2.9")
+            self.requires("wvware/1.2.9-vili")
 
     def build_requirements(self):
         self.test_requires("gtest/1.14.0")
