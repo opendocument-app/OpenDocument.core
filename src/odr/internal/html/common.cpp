2e95de18151e79ee9d45b8661b2ea642eccdb86b
Andreas Stefl <stefl.andreas@gmail.com>
2025-01-04 16:44:59 +0100

    fix warning

--- src/odr/internal/html/common.cpp
+++ src/odr/internal/html/common.cpp
@@ -68,7 +68,7 @@ HtmlResourceLocator html::local_resource_locator(const std::string &output_path,
     }
 
     if (!resource.is_relocatable() ||
-        resource.is_shipped() && !config.external_resource_path.empty()) {
+        (resource.is_shipped() && !config.external_resource_path.empty())) {
       auto resource_path = common::Path(config.external_resource_path)
                                .join(common::Path(resource.path()));
       if (config.relative_resource_paths) {
