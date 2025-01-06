ad2604f27f688e50074df24fbd758bf2eadfc45f
Andreas Stefl <stefl.andreas@gmail.com>
2025-01-06 10:47:28 +0100

    update refs; platform for docker

--- test/docker/compare_output_server.sh
+++ test/docker/compare_output_server.sh
@@ -16,5 +16,6 @@ fi
 docker run -ti \
   -v $(pwd):/repo \
   -p 8000:8000 \
+  --platform linux/amd64 \
   ghcr.io/opendocument-app/odr_core_test \
   python3 /repo/test/scripts/compare_output_server.py /repo/$REF /repo/$OBS --compare --driver $DRIVER --port 8000
