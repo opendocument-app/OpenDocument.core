9f004b7a3a22270ddb4743855643a2d36cf25aed
Andreas Stefl <stefl.andreas@gmail.com>
2025-01-08 21:10:31 +0100

    add util to get file size (#399)

--- src/odr/internal/util/file_util.hpp
+++ src/odr/internal/util/file_util.hpp
@@ -3,5 +3,9 @@
 #include <string>
 
 namespace odr::internal::util::file {
+
+std::size_t size(const std::string &path);
+
 std::string read(const std::string &path);
+
 } // namespace odr::internal::util::file
