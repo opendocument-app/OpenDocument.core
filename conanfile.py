e13eaed354ef38c415fb22641f8c32c5dc6629d4
Andreas Stefl <stefl.andreas@gmail.com>
2026-03-26 13:26:40 +0100

    bump some dependency versions

--- conanfile.py
+++ conanfile.py
@@ -39,13 +39,13 @@ class OpenDocumentCoreConan(ConanFile):
             del self.options.with_libmagic
 
     def requirements(self):
-        self.requires("pugixml/1.14")
+        self.requires("pugixml/1.15")
         self.requires("cryptopp/8.9.0")
         self.requires("miniz/3.0.2")
-        self.requires("nlohmann_json/3.11.3")
+        self.requires("nlohmann_json/3.12.0")
         self.requires("vincentlaucsb-csv-parser/2.3.0")
         self.requires("uchardet/0.0.8")
-        self.requires("utfcpp/4.0.4")
+        self.requires("utfcpp/4.0.9")
         if self.options.get_safe("with_pdf2htmlEX", False):
             self.requires("pdf2htmlex/0.18.8.rc1-odr-git-eb5d291")
         if self.options.get_safe("with_wvWare", False):
